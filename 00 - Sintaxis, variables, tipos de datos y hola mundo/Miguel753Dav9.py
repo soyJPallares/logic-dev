4a5d90805da6477c40e165e1a2e5987c57a24a72
Miguel753Dav9 <devmiguel753dav9@gmail.com>
2026-01-04 17:16:41 -0500

    Hola Mundo

---
+++ 00 - Sintaxis, variables, tipos de datos y hola mundo/Miguel753Dav9.py
@@ -0,0 +1,29 @@
+#https://www.python.org/
+'''
+Este es un comentario
+de varias lineas
+'''
+print("Hola, Python")
+print("     *")
+print("    *  *")
+print("   *    *")
+print("  *      *")
+#print(" *        *")
+print(" **********")
+print("    *  *")
+print("    *  *")
+print("    *  *")
+
+n = 1
+print(type(n))
+n = "Miguel"
+print(type(n))
+PI = 3,1416
+ 
+m = 1
+
+nombre = "Miguel"
+
+decimal = 1,70
+
+mayor = False
