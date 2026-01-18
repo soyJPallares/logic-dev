ac5300e52fd806776620fa5b46936ae3a9916983
Jonatan Pallares <162858325+soyJPallares@users.noreply.github.com>
2026-01-18 17:17:09 -0500

    Hola Mudo Java salida float con 2 decimales

--- Java/src/Topic00_SintaxVariablesDataTypeAndHelloWorld/HolaMundo.java
+++ Java/src/Topic00_SintaxVariablesDataTypeAndHelloWorld/HolaMundo.java
@@ -14,15 +14,21 @@ public class HolaMundo {
          */
 
         int n = 1; // Variable de tipo entero
+        System.out.println("N: " + n);
 
         final float pi = 3.1416f; // Constante de tipo float (usa decimales)
+        System.out.println("Pi: " + pi);
 
         int edad = 11; // Otra variable de tipo entero
+        System.out.println("Edad: " + edad);
 
-        String nombre = "Miguel"; // Variable de tipo Cadena de caracteres
+        String nombre = "Jonatan"; // Variable de tipo Cadena de caracteres
+        System.out.printf("Nombre: %s\n", nombre);
 
         float estatura = 1.70f; // Variable de tipo float (usa decimales)
+        System.out.printf("Estatura: %.2f\n", estatura);
 
         boolean mayor = false; // Variable de tipo Boolean (Usa logica booleana, Tiene dos valores posibles Verdadero o Falso)
+        System.out.printf("Mayor:  %b\n", mayor);
     }
 }
