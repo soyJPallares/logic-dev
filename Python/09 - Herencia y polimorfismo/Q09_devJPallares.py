e45ae22263612638f703a7d62d195c8b663f7829
Jonatan Pallares <162858325+soyJPallares@users.noreply.github.com>
2026-03-22 16:02:31 -0500

    Cambios en Q09 para mejorar herencia en la clase padre

--- Python/09 - Herencia y polimorfismo/Q09_devJPallares.py
+++ Python/09 - Herencia y polimorfismo/Q09_devJPallares.py
@@ -12,14 +12,9 @@
 #  * Dependiendo de su labor, tienen propiedades y funciones exclusivas de su
 #  * actividad, y almacenan los empleados a su cargo.
 
-def Menu():
-    print("1. Managemment")
-    print("2. Projects Managemment")
-    print("3. Development")
-    print("0. Quit")
 
-
-
+#--------------------------------------------------------------------------
+#--------------------------------------------------------------------------
 class Employee:
     def __init__(self, id: int, name: str, user: str):
         self.id: int = id
@@ -27,31 +22,29 @@ class Employee:
         self.user: str = user
         self.team: list = []
 
+#-------------------------------------
+    def add_team_member(self, member):
+        self.team.append(member)
+
+#-------------------------------------
+    def print_team(self):
+        print(f'Team: ')
+        for member in self.team: print(member.name)
 
 
+#--------------------------------------------------------------------------
+#--------------------------------------------------------------------------
 class Manager(Employee):
     def __init__(self, id, name, user, department: str):
         super().__init__(id, name, user)
         self.department: str = department
-        self.team: list = []
 
-
-    def assign_team(self, project_manager: str):
-        self.project_manager: str = project_manager
-        self.team.append(self.project_manager)
-
-
+#-------------------------------------
     def print_department(self):
         print(f'Department: {self.department} | Manager: {self.name}')
-        print(f'Team: ')
+        print(f'The {self.department} department manager, {self.name}, is managing the projects ')
 
-        managers_team: list = []
-        for member in range(len(self.team)):
-            managers_team.append(self.team[member].name)
-
-        print(managers_team)
-
-
+#-------------------------------------
     def approvals(self, amount:int, ticket:bool):
         self.amount: int = amount
         self.ticket: bool = ticket
@@ -59,7 +52,8 @@ class Manager(Employee):
         else: print(f'Amount: {self.amount} | Ticket: {self.ticket} | Bill - "Approved"')
 
 
-
+#--------------------------------------------------------------------------
+#--------------------------------------------------------------------------
 class ProjectManager(Employee):
     def __init__(self, id, name, user, project: str):
         super().__init__(id, name, user)
@@ -67,23 +61,11 @@ class ProjectManager(Employee):
         self.tasks: dict = {}
         self.team: list = []
 
-
-    def assign_team(self, programmer: str):
-        self.programmer: str = programmer
-        self.team.append(self.programmer)
-
-
+#-------------------------------------
     def print_project(self):
         print(f'Project: {self.project} | Project Manager: {self.name}')
-        print(f'Team: ')
-        
-        programmers_team: list = []
-        for member in range(len(self.team)):
-            programmers_team.append(self.team[member].name)
 
-        print(programmers_team)
-
-
+#-------------------------------------
     def assign_tasks(self, programmer: str, task: str):
         self.programmer: str = programmer
         self.task: str = task
@@ -91,82 +73,100 @@ class ProjectManager(Employee):
         self.tasks[self.programmer] = self.task
         print(f'Programmer: {self.programmer.name} | Task assigned: "{self.task}"')
 
-    # def printType(self):
-    #     print(type(self.tasks))
 
 
+#--------------------------------------------------------------------------
 class Programmer(Employee):
+#--------------------------------------------------------------------------
     def __init__(self, id, name, user, level: str, language: str):
         super().__init__(id, name, user)
         self.level: str = level
         self.language: str = language
 
+#-------------------------------------
+    def add_team_member(self, member:Employee):
+        print(f"Un programador no tiene empleados a su cargo. {member.name} no se añadirá.")
 
+#-------------------------------------
     def commit(self, message: str):
         self.message: str = message
         self.branch: str = self.user
         print(f'Commit: "{self.message}" | Branch: {self.branch} | Programmer: {self.name}')
 
-
 #--------------------------------------------------------------------------
+#--------------------------------------------------------------------------
+def Menu():
+    print("1. Managemment")
+    print("2. Projects Managemment")
+    print("3. Development")
+    print("0. Quit")
 
 
 def main():
 
-    my_mg: Manager = Manager(1, "Joshua Dax", "@jdax", "Operations")
+    my_manager: Manager = Manager(1, "Joshua Dax", "@jdax", "Operations")
 
-    my_pm_01: ProjectManager = ProjectManager(2, "John Doe", "@jdoe", "AlphaPrime")
-    my_pm_02: ProjectManager = ProjectManager(3, "Jane Dox", "@jdox", "BetaTax")
+    my_project_manager_01: ProjectManager = ProjectManager(2, "John Doe", "@jdoe", "AlphaPrime")
+    my_project_manager_02: ProjectManager = ProjectManager(3, "Jane Dox", "@jdox", "BetaTax")
 
-    my_prg_01: Programmer = Programmer(4, "Alice Fox", "@afox", "Senior", "python/FastApi")
-    my_prg_02: Programmer = Programmer(5, "Bob Smith", "@bsmith", "Junior", "javascript/React/")
-    my_prg_03: Programmer = Programmer(6, "Charlie Brown", "@cbrown", "Mid-level", "html/css")
-    my_prg_04: Programmer = Programmer(7, "Diana Prince", "@dprince", "Trainee", "python/FastApi")
-    my_prg_05: Programmer = Programmer(4, "Allen Foxter", "@afoxter", "Senior", "python/FastApi")
-    my_prg_06: Programmer = Programmer(5, "Bobie Smithson", "@bsmithson", "Junior", "javascript/React/")
-    my_prg_07: Programmer = Programmer(6, "Charles Boswell", "@cboswell", "Mid-level", "html/css")
-    my_prg_08: Programmer = Programmer(7, "Liv Tyler", "@ltyler", "Trainee", "python/FastApi")
-    # my_pm_01.print_type()
+    my_programmer_01: Programmer = Programmer(4, "Alice Fox", "@afox", "Senior", "python/FastApi")
+    my_programmer_02: Programmer = Programmer(5, "Bob Smith", "@bsmith", "Junior", "javascript/React/")
+    my_programmer_03: Programmer = Programmer(6, "Charlie Brown", "@cbrown", "Mid-level", "html/css")
+    my_programmer_04: Programmer = Programmer(7, "Diana Prince", "@dprince", "Trainee", "python/FastApi")
+    my_programmer_05: Programmer = Programmer(4, "Allen Foxter", "@afoxter", "Senior", "python/FastApi")
+    my_programmer_06: Programmer = Programmer(5, "Bobie Smithson", "@bsmithson", "Junior", "javascript/React/")
+    my_programmer_07: Programmer = Programmer(6, "Charles Boswell", "@cboswell", "Mid-level", "html/css")
+    my_programmer_08: Programmer = Programmer(7, "Liv Tyler", "@ltyler", "Trainee", "python/FastApi")
+    # my_project_manager_01.print_type()
     print('--------------------------------------------------------------------------')
 
-    my_mg.assign_team(my_pm_01)
-    my_mg.assign_team(my_pm_02)
-    my_mg.print_department()
-    my_mg.approvals(50000, True)
-    my_mg.approvals(60000, False)
+    my_manager.add_team_member(my_project_manager_01)
+    my_manager.add_team_member(my_project_manager_02)
+    my_manager.print_department()
+    my_manager.print_team()
+
+    my_manager.approvals(45000, False)
+    my_manager.approvals(60000, False)
+    my_manager.approvals(90000, True)
     print('--------------------------------------------------------------------------')
 
-    my_pm_01.assign_team(my_prg_01)
-    my_pm_01.assign_team(my_prg_02)
-    my_pm_01.assign_team(my_prg_03)
-    my_pm_01.assign_team(my_prg_04)
-    my_pm_01.print_project()
-    my_pm_01.assign_tasks(my_prg_01, "Implementar login")
-    my_pm_01.assign_tasks(my_prg_02, "Implementar registro")
-    my_pm_01.assign_tasks(my_prg_03, "Diseñar Maquetado")
-    my_pm_01.assign_tasks(my_prg_04, "Diseñar Base de Datos")
+    my_project_manager_01.print_project()
+    my_project_manager_01.add_team_member(my_programmer_01)
+    my_project_manager_01.add_team_member(my_programmer_02)
+    my_project_manager_01.add_team_member(my_programmer_03)
+    my_project_manager_01.add_team_member(my_programmer_04)
+    my_project_manager_01.print_team()
+
+    my_project_manager_01.assign_tasks(my_programmer_01, "Implementar login")
+    my_project_manager_01.assign_tasks(my_programmer_02, "Implementar registro")
+    my_project_manager_01.assign_tasks(my_programmer_03, "Diseñar Maquetado")
+    my_project_manager_01.assign_tasks(my_programmer_04, "Diseñar Base de Datos")
+    print('--------------------------------------------------------------------------')
+    my_programmer_01.add_team_member(my_programmer_02)
     print('--------------------------------------------------------------------------')
 
-    my_pm_02.assign_team(my_prg_05)
-    my_pm_02.assign_team(my_prg_06)
-    my_pm_02.assign_team(my_prg_07)
-    my_pm_02.assign_team(my_prg_08)
-    my_pm_02.print_project()
-    my_pm_02.assign_tasks(my_prg_05, "Diseñar Base de Datos / Backend")
-    my_pm_02.assign_tasks(my_prg_06, "Implementar login / Middleware")
-    my_pm_02.assign_tasks(my_prg_07, "Diseñar Maquetado / Frontend")
-    my_pm_02.assign_tasks(my_prg_08, "Diseñar / Implementar API REST")
+    my_project_manager_02.print_project()
+    my_project_manager_02.add_team_member(my_programmer_05)
+    my_project_manager_02.add_team_member(my_programmer_06)
+    my_project_manager_02.add_team_member(my_programmer_07)
+    my_project_manager_02.add_team_member(my_programmer_08)
+    my_project_manager_02.print_team()
+
+    my_project_manager_02.assign_tasks(my_programmer_05, "Diseñar Base de Datos / Backend")
+    my_project_manager_02.assign_tasks(my_programmer_06, "Implementar login / Middleware")
+    my_project_manager_02.assign_tasks(my_programmer_07, "Diseñar Maquetado / Frontend")
+    my_project_manager_02.assign_tasks(my_programmer_08, "Diseñar / Implementar API REST")
     print('--------------------------------------------------------------------------')
 
-    my_prg_01.commit("Login implemented")
-    my_prg_02.commit("Registration implemented")
-    my_prg_03.commit("Layout designed")
-    my_prg_04.commit("Database designed")
+    my_programmer_01.commit("Login implemented")
+    my_programmer_02.commit("Registration implemented")
+    my_programmer_03.commit("Layout designed")
+    my_programmer_04.commit("Database designed")
     print('--------------------------------------------------------------------------')
 
-    my_prg_05.commit("Database designed / Backend implemented")
-    my_prg_06.commit("Login implemented / Middleware implemented")
-    my_prg_07.commit("Layout designed / Frontend implemented")
-    my_prg_08.commit("API REST designed / implemented")
+    my_programmer_05.commit("Database designed / Backend implemented")
+    my_programmer_06.commit("Login implemented / Middleware implemented")
+    my_programmer_07.commit("Layout designed / Frontend implemented")
+    my_programmer_08.commit("API REST designed / implemented")
 
 main()
