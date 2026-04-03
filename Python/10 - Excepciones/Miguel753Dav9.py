91600bf2b3518c5159dc759c662b2e15f30e6c76
Miguel753Dav9 <devmiguel753dav9@gmail.com>
2026-04-03 12:10:55 -0500

    Los ejecicios de la clase 10

---
+++ Python/10 - Excepciones/Miguel753Dav9.py
@@ -0,0 +1,5 @@
+try:
+    print(10/1)
+    print([1,2,3,4][4])
+except Exception as e:
+    print(f"Se ha producido un error: {e}")
---
+++ Python/10 - Excepciones/Q10_Miguel753Dav9.py
@@ -0,0 +1,36 @@
+"""
+ * EJERCICIO:
+ * Explora el concepto de manejo de excepciones según tu lenguaje.
+ * Fuerza un error en tu código, captura el error, imprime dicho error
+ * y evita que el programa se detenga de manera inesperada.
+ * Prueba a dividir "10/0" o acceder a un índice no existente
+ * de un listado para intentar provocar un error.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Crea una función que sea capaz de procesar parámetros, pero que también
+ * pueda lanzar 3 tipos diferentes de excepciones (una de ellas tiene que
+ * corresponderse con un tipo de excepción creada por nosotros de manera
+ * personalizada, y debe ser lanzada de manera manual) en caso de error.
+ * - Captura todas las excepciones desde el lugar donde llamas a la función.
+ * - Imprime el tipo de error.
+ * - Imprime si no se ha producido ningún error.
+ * - Imprime que la ejecución ha finalizado.
+ """
+x = 10
+def prueba_de_errores(a : str) -> str:
+    if not isinstance(a, str):
+        raise TypeError(TypeError)
+    
+try:
+    prueba_de_errores(x)
+
+except NameError as e:
+    print(f"Tipo de error: {e}")
+except ZeroDivisionError as e:
+    print(f"Tipo de error: {e}")
+except TypeError as e:
+    print(f"Tipo de error: {e}")
+else:
+    print("No hay ningun error")
+finally:
+    print("Se ha terminado la prueba de errores")
