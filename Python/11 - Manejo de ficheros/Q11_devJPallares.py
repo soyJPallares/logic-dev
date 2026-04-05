b66cef341f0be1a0dd227089b2a114bb8ff4ea40
Jonatan Pallares <162858325+soyJPallares@users.noreply.github.com>
2026-04-05 13:04:16 -0500

    Se agregan Q11 de Python y se modifican archivos de manejo de ficheros

--- Python/11 - Manejo de ficheros/Q11_devJPallares.py
+++ Python/11 - Manejo de ficheros/Q11_devJPallares.py
@@ -14,31 +14,33 @@
 
 import os
 
-file_name = "ventas_soyJPallares.txt"
+file = "ventas_soyJPallares.txt"
 # ----------------------------------------------------------------------------------
 def crear_archivo() -> None:
 # ----------------------------------------------------------------------------------
-    with open(file_name, "w") as f:
-        f.write("Procesador AMD Ryzen 9, 15, 2450000.0\n")
-        f.write("Procesador Intel Core i9, 12, 2410000.0\n")
-        f.write("Tarjeta de Video NVIDIA RTX, 8, 6550000.0\n")
-        f.write("Tarjeta de Video AMD Radeon, 10, 3890000.0\n")
-        f.write("Memoria RAM Corsair DDR5, 45, 515000.0\n")
-        f.write("Disco SSD Samsung NVMe, 30, 720000.0\n")
-        f.write("Placa Base ASUS ROG, 7, 2050000.0\n")
-        f.write("Fuente de Poder EVGA, 22, 550000.0\n")
-        f.write("Gabinete NZXT, 14, 740000.0\n")
-        f.write("Refrigeración Líquida Corsair, 18, 775000.0\n")
-        f.write("Monitor LG UltraGear, 25, 1230000.0\n")
-        f.write("Teclado Mecánico Logitech, 20, 940000.0\n")
-        f.write("Mouse Razer DeathAdder, 35, 610000.0\n")
-        f.write("Disco Duro Seagate, 12, 820000.0\n")
-        f.write("Tarjeta de Sonido Creative, 5, 1430000.0\n")
-        f.write("Mando Xbox Wireless, 50, 245000.0\n")
-        f.write("Cámara Web Logitech, 40, 285000.0\n")
-        f.write("Auriculares HyperX Cloud, 28, 405000.0\n")
-        f.write("Pasta Térmica Thermal Grizzly, 100, 51000.0\n")
-        f.write("Adaptador Wi-Fi TP-Link, 15, 185000.0\n")
+    
+    if not os.path.exists(file):
+        with open(file, "a") as f:
+            f.write("Procesador AMD Ryzen 9, 15, 2450000.0\n")
+            f.write("Procesador Intel Core i9, 12, 2410000.0\n")
+            f.write("Tarjeta de Video NVIDIA RTX, 8, 6550000.0\n")
+            f.write("Tarjeta de Video AMD Radeon, 10, 3890000.0\n")
+            f.write("Memoria RAM Corsair DDR5, 45, 515000.0\n")
+            f.write("Disco SSD Samsung NVMe, 30, 720000.0\n")
+            f.write("Placa Base ASUS ROG, 7, 2050000.0\n")
+            f.write("Fuente de Poder EVGA, 22, 550000.0\n")
+            f.write("Gabinete NZXT, 14, 740000.0\n")
+            f.write("Refrigeración Líquida Corsair, 18, 775000.0\n")
+            f.write("Monitor LG UltraGear, 25, 1230000.0\n")
+            f.write("Teclado Mecánico Logitech, 20, 940000.0\n")
+            f.write("Mouse Razer DeathAdder, 35, 610000.0\n")
+            f.write("Disco Duro Seagate, 12, 820000.0\n")
+            f.write("Tarjeta de Sonido Creative, 5, 1430000.0\n")
+            f.write("Mando Xbox Wireless, 50, 245000.0\n")
+            f.write("Cámara Web Logitech, 40, 285000.0\n")
+            f.write("Auriculares HyperX Cloud, 28, 405000.0\n")
+            f.write("Pasta Térmica Thermal Grizzly, 100, 51000.0\n")
+            f.write("Adaptador Wi-Fi TP-Link, 15, 185000.0\n")
 
 # ----------------------------------------------------------------------------------
 def menu() -> None:
@@ -52,25 +54,52 @@ def menu() -> None:
     print("7. Salir")
 
 # ----------------------------------------------------------------------------------
-def añadir () -> None:
+def add () -> None:
 # ----------------------------------------------------------------------------------
-    print("Añadir producto")
-    nombre = input("Nombre: ")
-    cantidad = input("Cantidad: ")
-    precio = input("Precio: ")
+    print(f"{'-'+21}\n|  Añadir producto  |\n{'-'+21}")
+    nombre = input("Nombre del Producto: ")
+    cantidad = input("Cantidad Vendida: ")
+    precio = input("Precio Unitario: ")
 
-    with open(file_name, "a") as f:
+    with open(file, "a") as f:
         f.write(f"{nombre}, {cantidad}, {precio}.0\n")
 
 # ----------------------------------------------------------------------------------
-def consultar () -> None:
+def find () -> None:
 # ----------------------------------------------------------------------------------
-    pass
+    print(f"{'-'+21}\n|  Buscar producto  |\n{'-'+21}")
+    producto: str = input("Nombre del Producto: ")
+
+    with open(file, "r") as f:
+        lines: list[str] = f.readlines()
+
+    # Buscar la línea
+    for line in lines:
+        if producto in line:
+            print(f"Producto encontrado: {line.strip()}")
 
 # ----------------------------------------------------------------------------------
 def actualizar () -> None:
 # ----------------------------------------------------------------------------------
-    pass
+        print(f"{'-'+21}\n|  Actualizar producto  |\n{'-'+21}")
+        producto: str = input("Nombre del Producto: ")
+
+        with open(file, "r") as f:
+            lines: list[str] = f.readlines()
+
+        # Buscar y editar la línea
+        for i, line in enumerate(lines):
+            if producto in line:
+                print(f"Producto encontrado: {line.strip()}")
+                new_content: str = input("Nuevo contenido (dejar vacío para no cambiar): ")
+                # Reemplazar línea completa
+                # lines[i] = new_content.strip() + "\n"  
+                # Reemplazar solo una parte (Sólo la palabra o expresión)):
+                lines[i] = line.replace("contenido viejo", "contenido nuevo")
+
+        # # Reescribir el archivo
+        # with open(file, "w") as f:
+        #     f.writelines(lines)
 
 # ----------------------------------------------------------------------------------
 def eliminar () -> None:
@@ -96,5 +125,9 @@ def main() -> None:
         print(f.read())
 
     # os.remove(file_name)
+    add()
+
+    with open(file_name) as f:
+        print(f.read())
 
 main()
