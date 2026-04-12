23fe053f223f7369ef70181de3ca6090e8d4b12d
Jonatan Pallares <162858325+soyJPallares@users.noreply.github.com>
2026-04-11 22:58:25 -0500

    Tema 12 en python

---
+++ Python/12 - JSON y XML/soyJPallares.py
@@ -0,0 +1,56 @@
+import xml.etree.ElementTree as xml
+import json
+import os
+
+
+data: dict = {
+    "name": "Jonatan Pallares",
+    "age": 48,
+    "birth_date": "1977-06-12",
+    "dev_languages": ["Python", "Java"]
+}
+
+xml_file = 'devJPallares.xml'
+json_file = 'devJPallares.json'
+
+# XML
+
+def save_xml():
+    root = xml.Element('data')
+
+    for key, value in data.items():
+        child = xml.SubElement(root, key)
+        
+        if isinstance(value, list):
+            for item in value:
+                xml.SubElement(child, 'item').text = str(item)
+        else:
+            child.text = str(value)
+
+    tree = xml.ElementTree(root)
+    tree.write(xml_file)
+
+save_xml()
+
+with open(xml_file) as xml_data:
+    print(xml_data.read())
+    # tree = xml.parse(xml_data)
+    # root = tree.getroot()
+
+    # print("XML Data:")
+    # for child in root:
+    #     if child.tag == 'dev_languages':
+    #         print(f"{child.tag}: {[item.text for item in child]}")
+    #     else:
+    #         print(f"{child.tag}: {child.text}")
+
+
+# JSON
+
+with open(json_file, 'w') as json_data:
+    json.dump(data, json_data)
+
+with open(json_file) as json_data:
+    print(json_data.read())
+
+    
