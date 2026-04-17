9813e6ff26f9aacae4e43f7c9867e4e07c3b5aca
Jonatan Pallares <162858325+soyJPallares@users.noreply.github.com>
2026-04-16 23:53:41 -0500

    Agrego ruta al ejemplo para crear el archivo en el tema 11

--- Java/src/Topic11_FilesHandling/soyJPallares.java
+++ Java/src/Topic11_FilesHandling/soyJPallares.java
@@ -1,4 +1,29 @@
 package Topic11_FilesHandling;
+import java.io.File;
+import java.io.IOException;
+
+
 
 public class soyJPallares {
+
+
+    static File myObjFile = new File("./src/Topic11_FilesHandling/soyJPallares.txt");
+
+    static void createFile(){
+        try {
+            if (myObjFile.createNewFile()) {
+                System.out.println("\nFile Created: " + myObjFile.getName());
+            } else {
+                System.out.println("\nFile " + myObjFile.getName() + " already exists.");
+            }
+        } catch (IOException e){
+            System.out.println("\nAn error Ocurred: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args){
+        createFile();
+    }
+
 }
