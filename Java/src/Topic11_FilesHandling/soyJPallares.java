31b576f8e58a1585776aa7a3f0790faaed841e25
Jonatan Pallares <162858325+soyJPallares@users.noreply.github.com>
2026-04-17 23:04:34 -0500

    Se complementa temma 11 y se crea Q11 de Java

--- Java/src/Topic11_FilesHandling/soyJPallares.java
+++ Java/src/Topic11_FilesHandling/soyJPallares.java
@@ -1,20 +1,21 @@
 package Topic11_FilesHandling;
-import java.io.File;
-import java.io.IOException;
+import java.io.File;         // Import the File class
+import java.io.FileWriter;   // Import the FileWriter class
+import java.io.IOException;  // Import the IOException class
+import java.io.FileNotFoundException; // Import this class to handle errors
+import java.util.Scanner;             // Import the Scanner class to read text files
 
 
 
 public class soyJPallares {
 
-
-    static File myObjFile = new File("./src/Topic11_FilesHandling/soyJPallares.txt");
-
     static void createFile(){
+        File myObjFile = new File("./src/Topic11_FilesHandling/soyJPallares.txt");
         try {
             if (myObjFile.createNewFile()) {
-                System.out.println("\nFile Created: " + myObjFile.getName());
+                System.out.println("\nFile Created: \"" + myObjFile.getName() + "\"") ;
             } else {
-                System.out.println("\nFile " + myObjFile.getName() + " already exists.");
+                System.out.println("\nFile \"" + myObjFile.getName() + "\" already exists.");
             }
         } catch (IOException e){
             System.out.println("\nAn error Ocurred: " + e.getMessage());
@@ -22,8 +23,37 @@ public class soyJPallares {
         }
     }
 
-    public static void main(String[] args){
-        createFile();
+    static void writeFile() {
+        // FileWriter will be closed automatically here
+        try (FileWriter myWriter = new FileWriter("./src/Topic11_FilesHandling/soyJPallares.txt", true)) {
+            myWriter.write("Jonatan Pallares\n");
+            myWriter.write("48\n");
+            myWriter.write("Python y Java\n");
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 
+    static void readFile() {
+        File myObj = new File("./src/Topic11_FilesHandling/soyJPallares.txt");
+
+        // try-with-resources: Scanner will be closed automatically
+        try (Scanner myReader = new Scanner(myObj)) {
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args){
+        createFile();
+        writeFile();
+        readFile();
+    }
 }
